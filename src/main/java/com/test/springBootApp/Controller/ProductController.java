3b8cd873263d5d3b6444be73399b205e9ef03939
Max <37951897+MTDevJ@users.noreply.github.com>
2018-07-09 11:33:29 +0300

    Update list of imports

--- src/main/java/com/test/springBootApp/Controller/ProductController.java
+++ src/main/java/com/test/springBootApp/Controller/ProductController.java
@@ -1,22 +1,20 @@
 package com.test.springBootApp.Controller;
 
+import com.test.springBootApp.Entity.Category;
 import com.test.springBootApp.Entity.Product;
 import com.test.springBootApp.ErrorMessageClass;
-import com.test.springBootApp.Service.ActionMode;
-import com.test.springBootApp.Service.ProductActionServiceImpl;
+import com.test.springBootApp.Exception.NotFoundException;
+import com.test.springBootApp.Repository.CategoryRepository;
+import com.test.springBootApp.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
